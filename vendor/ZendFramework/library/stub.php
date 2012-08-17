f08127214c97440bfb7503ca23138b92f120e235
Vadim Leontiev <vadim@centralpowertrading.com>
2012-08-17 14:36:59 +0300

    Remove vendor folder

--- vendor/ZendFramework/library/stub.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/Zend/Loader/Autoloader.php';
-
-Zend_Loader_Autoloader::getInstance();
-Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
-// Finalize stumb
-__HALT_COMPILER();
