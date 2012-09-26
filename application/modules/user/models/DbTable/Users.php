276af663e433270cc23d94a38b913283879138d6
Vadim Leontiev <vadim@centralpowertrading.com>
2012-09-26 18:50:50 +0300

    Fix model mapper extends

--- application/modules/user/models/DbTable/Users.php
+++ application/modules/user/models/DbTable/Users.php
@@ -1,6 +1,15 @@
 <?php
 
-class User_Model_DbTable_Users extends Zend_Db_Table_Abstract
+/**
+ * DbTable for users table
+ * 
+ * @category Application
+ * @package Application_User
+ * @subpackage DbTable
+ * @license New BSD
+ * @author V.Leontiev <vadim.leontiev@gmail.com> 
+ */
+class User_Model_DbTable_Users extends Core_Db_Table_Abstract
 {
 
     protected $_name = 'users';
