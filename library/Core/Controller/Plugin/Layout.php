962f220ca6c3141666eee2ddd13ab69ff0fc5e80
Vadim Leontiev <vadim@centralpowertrading.com>
2013-04-26 18:56:00 +0300

    Create controller plugin for autoload layout

---
+++ library/Core/Controller/Plugin/Layout.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Enable layout for current module
+ *
+ * @category Core
+ * @package Core_Controller
+ * @subpackage Plugin
+ * @author V.Leontiev
+ */
+class Core_Controller_Plugin_Layout extends Zend_Controller_Plugin_Abstract
+{
+    
+}
