42b5c68b3a9812304ff3cd56c8b83bb62ccc025a
Vadim Leontiev <vadim@centralpowertrading.com>
2013-04-30 14:53:05 +0300

    Create layout plugin with autoloading existing layout for modules

--- library/Core/Controller/Plugin/Layout.php
+++ library/Core/Controller/Plugin/Layout.php
@@ -10,5 +10,37 @@
  */
 class Core_Controller_Plugin_Layout extends Zend_Controller_Plugin_Abstract
 {
-    
+
+    /**
+     * options from application.ini
+     * @var array
+     */
+    protected $_options = array();
+
+    /**
+     * Set options
+     * @param array $options
+     */
+    public function  __construct(Array $options = array())
+    {
+        $this->_options = $options;
+    }
+
+    /**
+     * Set layout for current module
+     *
+     * @param \Zend_Controller_Request_Abstract $request
+     */
+    public function postDispatch(\Zend_Controller_Request_Abstract $request)
+    {
+        $layout = Zend_Layout::getMvcInstance();
+        $moduleName = $request->getModuleName();
+
+        if (!strstr($layout->getLayout(), $moduleName)
+            && is_dir($layout->getLayoutPath() . DIRECTORY_SEPARATOR . $moduleName)
+        ) {
+            $layoutName = strstr($layout->getLayout(), '/');
+            $layout->setLayout($moduleName . $layoutName);
+        }
+    }
 }
