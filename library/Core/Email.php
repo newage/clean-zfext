9074b3213e1497231d71c5e8596e60721e31c6cf
vadim <vadim@Vadim-System-Home>
2013-06-11 20:22:53 +0300

    Update composer and create skeleton for email core

---
+++ library/Core/Email.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Create email apply templates and save to base
+ * Send saved emails
+ *
+ * @category Core
+ * @package Core_Email
+ * @author V.Leontiev <vadim.leontiev@gmail.com>
+ */
+class Core_Email
+{
+    public function createEmail()
+    {
+        
+    }
+    
+    public function sendEmails()
+    {
+        
+    }
+}
+
