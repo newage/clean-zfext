018ade19f789f69b6b900641d2b28b354ff2105c
Vadim Leontiev <vadim@centralpowertrading.com>
2013-07-05 14:51:26 +0300

    [#2] Feature (core); Added command-tool for create new models structure

--- library/Core/CodeGenerator/DbModel.php
+++ library/Core/CodeGenerator/DbModel.php
@@ -2,7 +2,7 @@
 
 /**
  * DbModel class generator
- * 
+ *
  * @category Core
  * @package Core_CodeGenerator
  * @author V.Leontiev
@@ -11,21 +11,28 @@
  */
 class Core_CodeGenerator_DbModel extends Core_CodeGenerator_Abstract
 {
- 
+
     const PART_DBTABLE = 'DbTable';
-    
+
     protected $_moduleName = null;
-    
+
+    /**
+     * Set used module name
+     *
+     * @param atring $moduleName
+     * @return \Core_CodeGenerator_DbModel
+     */
     public function setModule($moduleName)
     {
         $this->_moduleName = $moduleName;
+        return $this;
     }
-    
+
     /**
      * Get culumn in table and create methods in model
-     * 
-     * @param type $nameClass
-     * @param type $tableName
+     *
+     * @param string $modelName
+     * @param string $methods
      */
     public function generateModel($modelName, $methods)
     {
@@ -36,49 +43,34 @@ class Core_CodeGenerator_DbModel extends Core_CodeGenerator_Abstract
         $class->setName($className)
               ->setExtendedClass('Core_Model_Abstract')
               ->setDocblock($this->_generateDocBlock());
-        
+
         foreach ($methods as $fieldName) {
-            $class->setMethod($this->_createMethod('get' . ucfirst($fieldName)));
-            $class->setMethod($this->_createMethod('set' . ucfirst($fieldName)));
+
+            $class->setMethod(
+                $this->_createMethod(
+                    $this-> _generateMethodName($fieldName, 'get'),
+                    array(),
+                    'return $this->get("'.$fieldName.'");'
+                )
+            );
+
+            $class->setMethod(
+                $this->_createMethod(
+                    $this-> _generateMethodName($fieldName, 'set'),
+                    array(array('name' => 'value')),
+                    '$this->set("'.$fieldName.'", $value);' . "\n" . 'return $this;'
+                )
+            );
         }
-        
+
         $this->_generateFile($class->generate(), $path);
     }
-    
+
     /**
-     * Generate class name
-     * 
+     * Generate new model mapper class
+     *
      * @param string $modelName
-     * @return string
      */
-    protected function _generateClassName($modelName)
-    {
-        $className = array();
-        $className[] = !is_string($this->_moduleName) ? 'Application' : ucfirst($this->_moduleName);
-        $className[] = 'Model';
-        $className[] = $modelName;
-        
-        return implode('_', $className);
-    }
-    
-    /**
-     * Generate path to file
-     * 
-     * @param string $className
-     * @return string 
-     */
-    protected function _generatePath($className)
-    {
-        $module = $this->_moduleName !== null ? DIRECTORY_SEPARATOR . 'modules' .
-                DIRECTORY_SEPARATOR .$this->_moduleName : '';
-        $classExplode = explode('_', $className);
-        $file = array_pop($classExplode);
-        $path = APPLICATION_PATH . $module . DIRECTORY_SEPARATOR . 'models'
-                . DIRECTORY_SEPARATOR . $file . '.php';
-        
-        return $path;
-    }
-    
     public function generateMapper($modelName)
     {
         $class = new Zend_CodeGenerator_Php_Class();
@@ -88,7 +80,106 @@ class Core_CodeGenerator_DbModel extends Core_CodeGenerator_Abstract
         $class->setName($className)
               ->setExtendedClass('Core_Model_Mapper_Abstract')
               ->setDocblock($this->_generateDocBlock());
-        
+
         $this->_generateFile($class->generate(), $path);
+    }
+
+    /**
+     * Generate DbTable class
+     *
+     * @param string $modelName Model name
+     * @parem string $tableName Real table name
+     */
+    public function generateDbTable($modelName, $tableName)
+    {
+        $class = new Zend_CodeGenerator_Php_Class();
+        $className = $this->_generateClassName('DbTable_' . ucfirst($modelName));
+        $path = $this->_generatePath($className);
+        $modelClassName = $this->_generateClassName(ucfirst($modelName));
+
+        $class->setName($className)
+            ->setProperties(
+                array(
+                    array(
+                        'name' => '_name',
+                        'visibility' => 'protected',
+                        'defaultValue' => $tableName
+                    ),
+                    array(
+                        'name' => '_rowClass',
+                        'visibility' => 'protected',
+                        'defaultValue' => $modelClassName
+                    ),
+                    array(
+                        'name' => '_dependentTables',
+                        'visibility' => 'protected',
+                        'defaultValue' => array()
+                    ),
+                    array(
+                        'name' => '_referenceMap',
+                        'visibility' => 'protected',
+                        'defaultValue' => array()
+                    )
+                )
+            )
+            ->setExtendedClass('Core_Db_Table_Abstract')
+            ->setDocblock($this->_generateDocBlock());
+
+        var_dump('path',$path);
+        $this->_generateFile($class->generate(), $path);
+    }
+
+    /**
+     * Generate class name
+     *
+     * @param string $modelName
+     * @return string
+     */
+    protected function _generateClassName($modelName)
+    {
+        $className = array();
+        $className[] = !is_string($this->_moduleName) ? 'Application' : ucfirst($this->_moduleName);
+        $className[] = 'Model';
+        $className[] = $modelName;
+
+        return implode('_', $className);
+    }
+
+    /**
+     * Generate path to file
+     *
+     * @param string $className
+     * @return string
+     */
+    protected function _generatePath($className)
+    {
+        $module = $this->_moduleName !== null ? DIRECTORY_SEPARATOR . 'modules' .
+                DIRECTORY_SEPARATOR . $this->_moduleName : '';
+        $classExplode = explode('_', $className);
+        $modelPath = strstr($className, 'DbTable') ? 'models/DbTable' : 'models';
+        $file = array_pop($classExplode);
+        $path = APPLICATION_PATH . $module . DIRECTORY_SEPARATOR . $modelPath
+                . DIRECTORY_SEPARATOR . $file . '.php';
+
+        return $path;
+    }
+
+    /**
+     * Create camelCase name from sql field name
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function _generateMethodName($name, $prefix = '')
+    {
+        if (strstr($name, '_')) {
+            $function = function($part) {
+                return ucfirst($part);
+            };
+
+            $parts = explode('_', $name);
+            $name = implode('', array_map($function, $parts));
+        }
+        return $prefix . $name;
     }
 }
--- library/Core/Tool/Project/Provider/DbModel.php
+++ library/Core/Tool/Project/Provider/DbModel.php
@@ -1,30 +1,30 @@
 <?php
 
 /**
- * Create modet, dbTable, ModeMapper tool
- * 
- * @category Core
- * @package Core_Tool
- * @author V.Leontiev
+ * Create Model, dbTable, ModeMapper
  *
- * @version  $Id$
+ * @category Library
+ * @package Library_Tool_Project_Provider
+ * @author Vadim Leontiev <vadim.leontiev@gmail.com>
+ * @see https://bitbucket.org/newage/zf-tool
+ * @since php 5.1 or higher
  */
 class Core_Tool_Project_Provider_DbModel
-    extends Core_Tool_Project_Provider_Abstract
+    extends ZFTool_Tool_Project_Provider_Abstract
         implements Zend_Tool_Framework_Provider_Pretendable
 {
- 
+
     protected $_title = '[DbModel]';
-    
+
     /**
-     * @var Core_Migration_Manager 
+     * @var Core_Migration_Manager
      */
     protected $_manager = null;
-    
+
     /**
      * Initialize Core_Migration_Manager
      * Load profile and load development config
-     * 
+     *
      * @author V.Leontiev
      */
     public function initialize()
@@ -33,16 +33,16 @@ class Core_Tool_Project_Provider_DbModel
         $resources = $this->_app->getOption('resources');
         $this->_manager = new Core_Model_Manager($resources);
     }
-    
+
     /**
      * Create new model
-     * 
+     *
      * @author V.Leontiev
      */
     public function create($name, $actualTableName, $module = null)
     {
         $this->_manager->create($name, $actualTableName, $module);
-        $this->_print('Create new Model and ModelMapper', array('color' => 'green'));
+        $this->_print('Created Model / ModelMapper / DbTable', array('color' => 'green'));
     }
 }
 
