a4f14450c06bbebdba1440fe6994807cc805dc9c
Vadim Leontiev <vadim@centralpowertrading.com>
2013-07-22 18:41:24 +0300

    Feature (core); added compare date method to rowset object

--- library/Core/Db/Table/Rowset.php
+++ library/Core/Db/Table/Rowset.php
@@ -11,5 +11,39 @@
  */
 class Core_Db_Table_Rowset extends Zend_Db_Table_Rowset_Abstract
 {
+    const DATE_FEATURE = 0;
+    const DATE_NOW = 1;
+    const DATE_PAST = 2;
 
+    /**
+     * Count date for rowset
+     *
+     * @param string $methodName
+     * @param int $type Return type (0-feature, 1-now, 2-past)
+     * @return array
+     */
+    public function dateCount($methodName, $type = self::DATE_FEATURE)
+    {
+        $result = array();
+        $date = Zend_Date::now();
+        foreach ($this as $row) {
+            $compare = $date->compareDate($row->{$methodName}());
+            switch ($compare) {
+                case -1:
+                    $result[self::DATE_FEATURE]++;
+                    break;
+                case 0:
+                    $result[self::DATE_NOW]++;
+                    break;
+                case 1:
+                    $result[self::DATE_PAST]++;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        return $result[$type];
+    }
 }
